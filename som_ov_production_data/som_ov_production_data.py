1b048b2db2a6d60fbcfa6498d0c910f135705565
David García Garzón <david.garcia@somenergia.coop>
2024-02-29 09:22:01 +0100

    🚧🐛 Only one point per hour, prioritize real measures
    Co-authored-by: Beatrigz <beatriz.plana@somenergia.coop>
    Co-authored-by: javikalsan <jcalzadosanchez@gmail.com>

--- som_ov_production_data/som_ov_production_data.py
+++ som_ov_production_data/som_ov_production_data.py
@@ -102,13 +102,14 @@ class SomOvProductionData(osv.osv_memory):
         SQL query breakdown:
 
             1. Common Table Expressions (CTEs):
-                * `filtered_data`: Filters data from `giscere_mhcil` based on specified criteria
+                * `filtered_data`: Filters data from `giscere_mhcil` based on:
                   (cil, first_timestamp_utc, and last_timestamp_utc).
                 * `filled_data`: Generates a series of timestamps between `first_timestamp_utc`
                   and `last_timestamp_utc`, and fills in NULL values for `ae`, `maturity`, and
                   `type_measure` where there are gaps in data.
-                * `final_data`: Joins the filtered and filled data, ensuring no gaps exist.
-                * `ranked_data`: Assigns a rank to each record based on the maturity level.
+                * `joined_data`: Joins the filtered and filled data, ensuring no gaps exist.
+                * `ranked_data`: Adds a puntuation field to prioritize data for a same
+                  timestamp depending ong `type_measure` and `maturity`
 
             2. Main Query:
                 * Selects aggregated data as a JSON object.
@@ -131,7 +132,7 @@ class SomOvProductionData(osv.osv_memory):
             '''
                 WITH filtered_data AS (
                     SELECT
-                        "timestamp" AT TIME ZONE 'UTC' AS "timestamp",
+                        u"timestamp" AT TIME ZONE 'UTC' AS "timestamp",
                         ae,
                         maturity,
                         type_measure
@@ -158,7 +159,7 @@ class SomOvProductionData(osv.osv_memory):
                     WHERE
                         fd."timestamp" IS NULL
                 ),
-                final_data AS (
+                joined_data AS (
                     SELECT
                         COALESCE(fd."timestamp", fd2."timestamp") AS "timestamp",
                         COALESCE(fd.ae, NULL) AS ae,
@@ -172,17 +173,26 @@ class SomOvProductionData(osv.osv_memory):
                 ranked_data AS (
                     SELECT
                         *,
-                        RANK() OVER (PARTITION BY "timestamp" ORDER BY
-                            CASE
-                                WHEN maturity = 'HC' THEN 1
-                                WHEN maturity = 'HP' THEN 2
-                                WHEN maturity = 'H3' THEN 3
-                                WHEN maturity = 'H2' THEN 4
-                                ELSE 5
-                            END
-                        ) AS maturity_rank
+                        CASE
+                            WHEN maturity = 'HC' THEN 1
+                            WHEN maturity = 'HP' THEN 2
+                            WHEN maturity = 'H3' THEN 3
+                            WHEN maturity = 'H2' THEN 4
+                            ELSE 5
+                        END + CASE
+                            WHEN type_measure in ('R', 'L') THEN 0
+                            WHEN type_measure in ('E', 'M') THEN 10
+                            ELSE 20 -- NULL or unexpected
+                        END AS ranking
                     FROM
-                        final_data
+                        joined_data
+                ),
+                collapsed_data AS (
+                    SELECT DISTINCT ON("timestamp")
+                        *
+                    FROM
+                        ranked_data
+                    ORDER BY "timestamp" ASC, ranking ASC
                 )
                 SELECT
                     JSON_BUILD_OBJECT(
@@ -198,9 +208,7 @@ class SomOvProductionData(osv.osv_memory):
                         'maturity', COALESCE(ARRAY_AGG(maturity ORDER BY "timestamp" ASC), ARRAY[]::text[])
                     ) AS data
                 FROM
-                    ranked_data
-                WHERE
-                    maturity_rank = 1;
+                    collapsed_data
             ''',
             {
                 'cil': contract.cil.name,
